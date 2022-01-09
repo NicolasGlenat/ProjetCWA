acb00d2f847d63307b5c17970ba3660a69ba63f0
Nicolasglenat <nicolasglenat@orange.fr>
2022-01-09 12:29:56 +0100

    refonte graphique1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AcceuilComponent } from "./acceuil/acceuil.component";
+
+
+const routes: Routes = [
+  {path:'ac',component: AcceuilComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
